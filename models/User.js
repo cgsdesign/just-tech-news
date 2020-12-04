475cabdee0dac25c2058069d5a4a747022e29bbe
Cat <CGraham@CatherinesMBP2.attlocal.net>
2020-12-04 16:57:35 -0600

    added encription to pws

--- models/User.js
+++ models/User.js
@@ -1,6 +1,6 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
-
+const bcrypt = require('bcrypt');
 
 
 // define table columns and configuration
@@ -67,6 +67,29 @@ User.init(
       }
     },
     {
+    //.then version
+    //   hooks: {
+    //     // set up beforeCreate lifecycle "hook" functionality
+    //     //encrypts pw
+    //     beforeCreate(userData) {
+    //         return bcrypt.hash(userData.password, 10).then(newUserData => {
+    //         return newUserData
+    //         });
+    //     }
+    //   },
+    //async awaite version
+    hooks: {
+        // set up beforeCreate lifecycle "hook" functionality
+        async beforeCreate(newUserData) {
+            newUserData.password = await bcrypt.hash(newUserData.password, 10);
+            return newUserData;
+        },
+          // set up beforeUpdate lifecycle "hook" functionality
+        async beforeUpdate(updatedUserData) {
+            updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+            return updatedUserData;
+        }
+    },
       sequelize,
       timestamps: false,
       freezeTableName: true,
